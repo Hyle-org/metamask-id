8bcc3a49db234a7d711407150c636ebf84f29189
Bertrand Darbon <bertrand@hyle.eu>
2025-02-13 10:28:07 +0100

    fix packages

--- hyle-snap/packages/snap/snap.config.ts
+++ hyle-snap/packages/snap/snap.config.ts
@@ -1,10 +1,6 @@
 import { merge, SnapConfig } from '@metamask/snaps-cli';
 import { resolve } from 'path';
 
-require('@babel/core').transformSync('code', {
-  plugins: [['@babel/plugin-proposal-decorators', { version: '2023-11' }]],
-});
-
 const config: SnapConfig = {
   bundler: 'webpack',
   input: resolve(__dirname, 'src/index.tsx'),
